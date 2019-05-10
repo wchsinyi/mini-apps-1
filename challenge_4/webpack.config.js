252b4e973d816a9c2b997798f92a2dc51059578a
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-05-09 21:55:45 -0700

    Temp

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,14 @@
+    module.exports = {
+      module: {
+        rules: [
+          {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          }
+        ]
+      }
+    };
+
